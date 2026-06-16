ceb8c58fb84b6dad2866eb079c93771047d10700
Vikas Kumar Wanchoo <wanchoo@live.in>
2026-06-16 20:13:59 +0530

    Fix assertions in test_home_endpoint and test_health_endpoint

--- test_app.py
+++ test_app.py
@@ -3,15 +3,17 @@ from app import app
 def test_home_endpoint():
     client = app.test_client()
     response = client.get("/")
+
     assert response.status_code == 200
 
     data = response.get_json()
-    assert data["source_of_truth"] == "GitHub"
     assert data["ci_engine"] == "Azure DevOps Pipeline"
+    assert data["source_of_truth"] == "GitHub"
 
 def test_health_endpoint():
     client = app.test_client()
     response = client.get("/health")
+
     assert response.status_code == 200
 
     data = response.get_json()
