c6b494bdfa582452d7a5fa88d5ec38b327ce4293
Vikas Kumar Wanchoo <wanchoo@live.in>
2026-06-16 19:08:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return jsonify({
-        "message": "Hello from GitHub source repo",
+        "message": "message": "Hello from GitHub PR validation",
         "ci_engine": "Azure DevOps Pipeline",
         "source_of_truth": "GitHub"
     })
